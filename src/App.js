18da221266d321306673ad134a8d8137f203369b
vannakdy <dyvannak1997@gmail.com>
2022-08-17 22:55:13 +0700

    test

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import HomePage from "./page/HomePage";
 import AboutPage from "./page/AboutPage";
 import LoginPage from "./page/LoginPage";
 import RegisterPage from "./page/RegisterPage";
+import ProductPage from "./page/ProductPage";
 import Route404Page from "./page/Route404Page";
 import Header1 from "./component/header/Header1";
+import "./App.css";
 import {BrowserRouter,Route,Routes} from "react-router-dom";
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
       <Header1 />
       <Routes>
         <Route path="/" element={<HomePage/>} />
+        <Route path="/product" element={<ProductPage/>} />
         <Route path="/about" element={<AboutPage/>} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/register" element={<RegisterPage/>} />
--- src/component/header/Header1.js
+++ src/component/header/Header1.js
@@ -5,6 +5,7 @@ function Header1() {
             <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
+            <li><Link to="/product">Product</Link></li>
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/register">Register</Link></li>
             <li><Link to="/invoice">invoice</Link></li>
